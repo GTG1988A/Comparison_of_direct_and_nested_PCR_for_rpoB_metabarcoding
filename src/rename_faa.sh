389225f00bf4d2cb6af4c71e18aacbd276114d95
Gabryelle Agoutin <83345122+GTG1988A@users.noreply.github.com>
2024-07-08 16:49:10 +0200

    Delete src directory

--- src/rename_faa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in /path_to_data/protein_seq/arc_refseq_cg/2024-02-05_09-01-02/files/*.faa.gz; do
-
-    filename=$(basename "$file" .faa.gz)
-
-    zcat "$file" | awk '/^>/ {print $1; next} {print}' > "/path_to_data/protein_seq/arc_refseq_cg/2024-02-05_09-01-02/files/modified/${filename}_modified.faa"
-done
